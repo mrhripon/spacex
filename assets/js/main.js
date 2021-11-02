b23feaf69f705316a154a16c0af6bf5d3e75577c
Mrh <mrhripon98@gmail.com>
2021-11-02 23:44:03 +0600

    Complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -86,9 +86,56 @@
 
 
 
+         // Query for countdown timer
+      function getTimeRemaining(endtime) {
+        var t = Date.parse(endtime) - Date.parse(new Date());
+        var seconds = Math.floor((t / 1000) % 60);
+        var minutes = Math.floor((t / 1000 / 60) % 60);
+        var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+        var days = Math.floor(t / (1000 * 60 * 60 * 24));
+        return {
+          'total': t,
+          'days': days,
+          'hours': hours,
+          'minutes': minutes,
+          'seconds': seconds
+        };
+      }
+      
+      function initializeClock(id, endtime) {
+        var clock = document.getElementById(id);
+        // var daysSpan = clock.querySelector('.days');
+        var hoursSpan = clock.querySelector('.hours');
+        var minutesSpan = clock.querySelector('.minutes');
+        var secondsSpan = clock.querySelector('.seconds');
+      
+        function updateClock() {
+          var t = getTimeRemaining(endtime);
+      
+          // daysSpan.innerHTML = t.days;
+          hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+          minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+          secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+      
+          if (t.total <= 0) {
+            clearInterval(timeinterval);
+          }
+        }
+      
+        updateClock();
+        var timeinterval = setInterval(updateClock, 1000);
+      }
+      
+      var deadline = new Date(Date.parse(new Date()) + 360 * 24 * 60 * 60 * 1000);
+      initializeClock('clockdiv', deadline);
+
+
+
+
          //   Query For scroll back to top 
   let back = $('.back-to-top');
-  back.on('click', function () {
+  back.on('click', function (e) {
+    e.preventDefault();
       $('html, body').animate({
           scrollTop: 0,
       }, 900);
